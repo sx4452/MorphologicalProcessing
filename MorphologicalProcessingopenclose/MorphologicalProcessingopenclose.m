0eb3aa4468bb87ac290f2f9fabe09e2f0d7d090c
sx4452 <zb.benzhang@gmail.com>
2015-11-11 13:42:32 +0800

    20151111

--- MorphologicalProcessingopenclose/MorphologicalProcessingopenclose.m
+++ MorphologicalProcessingopenclose/MorphologicalProcessingopenclose.m
@@ -1,13 +1,28 @@
 function MorphologicalProcessingopenclose
 I = imread('wordbook.png');
-I = logical(rgb2gray(I));
-SEdiamond = strel('diamond', 10);
-SEdisk = strel('disk', 10, 0);
-SEline = strel('line', 10, 0);
-SEocragon = strel('octagon', 10);
-SEpair = strel('')
+I = rgb2gray(I);
+I = im2double(I);
+I(I>0.9) = 1;
+I(I<=0.9) = 0;
+I = logical(I);
 
+SEdiamond = strel('diamond', 3);
+SEdisk = strel('disk', 3, 0);
+SEline = strel('line', 3, 0);
+SEoctagon = strel('octagon', 3);
+SEpair = strel('pair', [3, 3]);
+SEperiodicline = strel('periodicline', 3, [1, -2]);
+SErectangle = strel('rectangle', [3, 2]);
+SEsquare = strel('square', 3);
+SEarbitrary = strel('arbitrary',[1 0 0; 1 0 0; 1 0 1]);
 
+name = {'diamond', 'disk', 'line', 'octagon', 'pair', 'periodicline', 'rectangle', 'square', 'arbitrary'};
+for i = 1:9
+    outname = strcat('I', name{i});
+    SEname = strcat('SE', name{i});
+    eval(strcat(outname,' = imclose(I,', SEname, ');'));
+    outfilename = strcat('wordbookclose',name{i},'.jpg');
+    eval(strcat('imwrite(', outname, ',outfilename);'));
 end
 %I = im2double(I);
 %{
